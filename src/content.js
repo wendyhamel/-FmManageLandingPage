6e1ea0f81fbef4236dbd857beb9a92ee3c92718a
wendyhamel <whamel@it4web.nl>
2024-09-10 15:16:18 +0200

    Working on layout and styling of testimonial slider

--- src/content.js
+++ src/content.js
@@ -2,6 +2,7 @@ window.showContent = function () {
 	return {
 		menuOpened: false,
 		subMenuOpened: false,
+		activeTm: 'tm02',
 		menuItems: [
 			{
 				title: 'Pricing',
@@ -43,21 +44,25 @@ window.showContent = function () {
 		],
 		testimonials: [
 			{
+				id: 'tm01',
 				image: "./images/avatar-anisha.png",
 				name: 'Anisha Li',
 				description: '“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”',
 			},
 			{
+				id: 'tm02',
 				image: "./images/avatar-ali.png",
 				name: 'Ali Bravo',
 				description: '“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”',
 			},
 			{
+				id: 'tm03',
 				image: "./images/avatar-richard.png",
 				name: 'Richard Watts',
 				description: '“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”',
 			},
 			{
+				id: 'tm04',
 				image: "./images/avatar-shanai.png",
 				name: 'Shanai Gough',
 				description: '“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”',
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       'sans': ['Be\\ Vietnam\\ Pro', 'sans-serif']
     },
     extend: {
+      fontSize: {
+        '3.5xl': '2rem'
+      },
+      letterSpacing: {
+        'tight-md': '-0.03em'
+      },
+      lineHeight: {
+        '11': '2.8rem'
+      },
       colors: {
         red: 'hsl(12, 88%, 59%)',
         'red-pale': 'hsl(13, 100%, 96%)',
